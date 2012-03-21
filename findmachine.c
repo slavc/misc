c87acdbc2311c95a1d5a82c7f491f5f0ad107c06
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2012-03-21 17:27:19 +0200

    findmachine

--- findmachine.c
+++ findmachine.c
@@ -6,73 +6,101 @@
 #include <err.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 
-#include "findmachine.h"
+#define DEFAULT_PORT 1234
 
-#define IP_ADDR(a, b, c, d) htonl(((((a) << 8) | (b)) << 8 | (c)) << 8 | d)
+extern char *optarg;
+extern int optind, opterr, optopt;
 
 int
 main(int argc, char **argv)
 {
-    int s;
-    struct sockaddr_in sin, peer;
-    socklen_t peer_len;
-    const char *hostname;
-    struct {
-        unsigned int a;
-        unsigned int b;
-        unsigned int c;
-        unsigned int d;
-    } ip;
-    char buf[HOST_NAME_MAX + 1];
+    int              ch;
+    extern int       optind;
+    int              listen_mode;
+    const char      *addr = NULL;
+    unsigned short   port;
 
-    if (argc < 3) {
-        usage();
-        return 1;
+    listen_mode = 0;
+    port = DEFAULT_PORT;
+
+    while ((ch = getopt(argc, argv, "hl")) != -1) {
+        switch (ch) {
+        case 'h':
+            usage();
+            exit(0);
+        case 'l':
+            listen_mode = 1;
+            break;
+        default:
+            usage();
+            exit(1);
+            break;
+        }
     }
 
-    if (sscanf(argv[1], "%u.%u.%u", &ip.a, &ip.b, &ip.c) != 3) {
-        usage();
-        return 1;
-    }
-    hostname = argv[2];
+    argc -= optind;
+    argv += optind;
 
-    s = socket(AF_INET, SOCK_DGRAM, 0);
-    if (s == -1)
-        err(1, "socket");
-
-    bzero(&sin, sizeof(sin));
-    sin.sin_family = AF_INET;
-    sin.sin_port = htons(PORT);
-    sin.sin_addr.s_addr = htonl(INADDR_ANY);
-
-    if (bind(s, (const struct sockaddr *) &sin, sizeof(sin)))
-        err(1, "bind");
-
-#define MIN_ADDR 1
-#define MAX_ADDR 255
-
-    for (ip.d = MIN_ADDR; ip.d < MAX_ADDR; ++ip.d) {
-        bzero(&sin, sizeof(sin));
-        sin.sin_family = AF_INET;
-        sin.sin_port = htons(PORT);
-        sin.sin_addr.s_addr = htonl(ip.a, ip.b, ip.c, ip.d);
-
-        /* It doesn't matter what exactly we send, just throw a byte */
-        if (sendto(s, buf, 1, (const struct sockaddr *) &sin, sizeof(sin)))
-            err(1, "sendto");
+    if (argc > 0) {
+        addr = *argv;
+        --argc;
+        ++argv;
     }
 
-    /* ???????? */
+    if (argc > 0) {
+        if (sscanf(*argv, "%hu", &port) < 1) {
+            usage();
+            exit(1);
+        }
+    }
 
-    close(s);
+    if (listen_mode)
+        findmachined(port);
+    else {
+        if (addr == NULL) {
+            usage();
+            exit(1);
+        }
+        findmachine(addr, port);
+    }
 
     return 0;
 }
 
 void
-usage(void)
+findmachined(unsigned short port)
 {
-        printf("usage: findmachine <subnet> <hostname>\n");
+    int                  s;
+    struct sockaddr_in   my_addr, peer_addr;
+    size_t               peer_addr_size;
+    char                 rcvbuf[1];
+    const size_t         rcvbuf_size = sizeof(rcvbuf);
+    char                *sndbuf = NULL;
+    size_t               sndbuf_size = 0;
+    ssize_t              n_bytes;
+
+    s = socket(AF_INET, SOCK_DGRAM, 0);
+    if (s == -1)
+        err(1, "socket");
+
+    bzero(&my_addr, sizeof(my_addr));
+    my_addr.sin_port = hton(port);
+    my_addr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(s, &my_addr, sizeof(my_addr)))
+        err(1, "bind");
+
+    for ( ;; ) {
+        peer_addr_size = sizeof(peer_addr);
+        n_bytes = recvfrom(s, rcvbuf, rcvbuf_size, &peer_addr, &peer_addr_size);
+        if (n_bytes == -1)
+            err(1, "recvfrom");
+        else if (n_bytes == 0)
+            continue;
+
+        /* TODO get hostname and the list of our ip addresses and reply */
+    }
+        
+    return 0;
 }
