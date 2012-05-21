cebce8395b8fa917db6d207d481e39a0a252c7fb
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2012-05-21 12:57:58 +0300

    update fedora setup script

--- dotfiles/setup_fedora.sh
+++ dotfiles/setup_fedora.sh
@@ -38,6 +38,17 @@ if [ `id -u` -ne 0 ]; then
     curl -s -o ~/tmp/darklime.tgz http://art.gnome.org/download/themes/gtk2/1364/GTK2-ClearlooksDarkLime.tar.gz && mkdir -p ~/.themes && tar -C ~/.themes -zxf ~/tmp/darklime.tgz
     curl -s -o ~/tmp/darkilouche.tbz2 http://art.gnome.org/download/themes/gtk2/1285/GTK2-Darkilouche.tar.bz2 && mkdir -p ~/.themes && tar -C ~/.themes -jxf ~/tmp/darkilouche.tbz2
 
+
+    svn co https://core.fluendo.com/gstreamer/svn/trunk/ ~/src/fluendo
+    cd ~/src/fluendo/gst-fluendo-mp3
+    ./autogen.sh
+    ./configure --with-liboil
+    make
+    dstdir=$HOME/.gstreamer-0.10/plugins
+    mkdir -p "$dstdir"
+    cp src/.libs/libgstflump3dec.so "$dstdir"
+
+
 else # perform actions which require root privileges
     yum -y upgrade || error 'failed to upgrade packages'
     yum -y erase 'PackageKit-command-not-found' || error 'failed to uninstall PackageKit-command-not-found'
@@ -50,6 +61,8 @@ else # perform actions which require root privileges
         ctags \
         cvs \
         freetype-devel \
+	automake \
+	autoconf \
         gcc \
         gcc-c++ \
         gimp \
@@ -61,6 +74,12 @@ else # perform actions which require root privileges
         libXft-devel \
         libXinerama-devel \
         libXrandr-devel \
+        libXv-devel \
+        gstreamer-devel \
+        gstreamer-plugins-base-devel \
+        pulseaudio-libs-devel \
+        gtk2-devel \
+        gtk3-devel \
         make \
         man-pages \
         mupdf \
@@ -89,6 +108,11 @@ else # perform actions which require root privileges
         gtk-chtheme \
         tango-icon-theme \
         tango-icon-theme-extras \
+        yasm \
+        yasm-devel \
+        liboil \
+        liboil-devel \
+        libtool \
     || error 'failed to install useful packages'
 fi
 
