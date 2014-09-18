7b25fb139ea82d3e3719bfce8c6f65560b2ee4c0
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2014-09-18 23:22:20 +0300

    Fix battery.sh.
    Names of files changed: s/^energy/charge/.

--- home.bin/battery.sh
+++ home.bin/battery.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-energy_now=`cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/energy_now'`
-energy_full=`cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/energy_full'`
+charge_now=`cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/charge_now'`
+charge_full=`cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/charge_full'`
 status=`cat '/sys/bus/acpi/drivers/battery/PNP0C0A:00/power_supply/BAT0/status'`
 
 charge=`bc << EOF
 scale=2
-x=($energy_now/$energy_full)*100
+x=($charge_now/$charge_full)*100
 x
 EOF`
 
