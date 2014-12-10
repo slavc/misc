dadf0a67816c3fb9738cceb47eba8df0581b5fb6
Sviatoslav Chagaev <sviatoslav.chagaev@gmail.com>
2014-12-10 23:05:46 +0200

    Use the new prompt command for PS1.

--- dotfiles/zzz_local.sh
+++ dotfiles/zzz_local.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-ps1fg=30
+ps1fg=34
 if [ `id -u` -eq 0 ]; then
     ps1fg=31
 fi
-export COLUMNS
-export PWD
-PS1='\[\e[1;'$ps1fg';47m\]`awk "BEGIN { n_cols = int(ENVIRON[\"COLUMNS\"]); txt = ENVIRON[\"PWD\"]; gsub(\"^\" ENVIRON[\"HOME\"], \"~\", txt); line = \"                                                                      \"; line_len = length(line); n = n_cols - length(txt); printf(\"%s\", txt); for (i = 0; i < n; i += line_len) { printf(\"%s\", substr(line, 1, n - i)); } }"`\[\e[0m\]\n'
+if [ -x /usr/local/bin/prompt ]; then
+    export COLUMNS
+    PS1='$(/usr/local/bin/prompt -B -f'${ps1fg}' -b white [\j]\ \w\%\t)\n'
+else
+    PS1='\[\e[1;'${ps1fg}'m\]\w:\j \[\e[0m\]'
+fi
+unset ps1fg
 
-case "$TERM" in
-*rxvt*|*xterm*)
-    PROMPT_COMMAND='printf "\033]0;'
-    if [ `id -u` -eq 0 ]; then
-        PROMPT_COMMAND="${PROMPT_COMMAND}#"
-    else
-        PROMPT_COMMAND="${PROMPT_COMMAND}\$"
-    fi
-    PROMPT_COMMAND="${PROMPT_COMMAND}"' ${PWD/#${HOME}/~}\007"'
-    export PROMPT_COMMAND
-    ;;
-esac
+PROMPT_COMMAND='printf "\033]0;'
+if [ `id -u` -eq 0 ]; then
+    PROMPT_COMMAND="${PROMPT_COMMAND}#"
+else
+    PROMPT_COMMAND="${PROMPT_COMMAND}\$"
+fi
+PROMPT_COMMAND="${PROMPT_COMMAND}"' ${PWD/#${HOME}/~}\007"'
+export PROMPT_COMMAND
+
 PAGER=/usr/bin/less
 LESS=iSR
 VISUAL=/usr/bin/vim
 EDITOR=$VISUAL
 
-export PS1 PAGER LESS VISUAL EDITOR
+export PAGER LESS VISUAL EDITOR
+
